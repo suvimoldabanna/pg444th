5df9ac7c6ef36a904313af82e3619b77f6160d6a
suvimoldabanna <152445319+suvimoldabanna@users.noreply.github.com>
2025-02-28 13:55:00 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,18 +4,15 @@ import React from "react";
 import Header from "@/components/Header";
 import BackgroundImage from "@/components/BackgroundImage";
 import Slides from "@/components/Slides";
-import SlideInfo from "@/components/SlideInfo";
 import Controls from "@/components/Controls";
 
 const inter = Righteous({
   subsets: ["latin"],
   weight: ["400"],
 });
+
 export type Data = {
   img: string;
-  title: string;
-  description: string;
-  location: string;
 };
 
 export type CurrentSlideData = {
@@ -25,36 +22,30 @@ export type CurrentSlideData = {
 
 export default function Home() {
   const [data, setData] = React.useState<Data[]>(sliderData.slice(1));
-  const [transitionData, setTransitionData] = React.useState<Data>(
-    sliderData[0]
-  );
-  const [currentSlideData, setCurrentSlideData] =
-    React.useState<CurrentSlideData>({
-      data: initData,
-      index: 0,
-    });
+  const [transitionData, setTransitionData] = React.useState<Data>(sliderData[0]);
+  const [currentSlideData, setCurrentSlideData] = React.useState<CurrentSlideData>({
+    data: initData,
+    index: 0,
+  });
 
   return (
     <main
-      className={`
-       ${inter.className}
-        relative min-h-screen select-none overflow-hidden text-white antialiased`}
+      className={`${
+        inter.className
+      } relative min-h-screen select-none overflow-hidden text-white antialiased`}
     >
       <AnimatePresence>
         <BackgroundImage
           transitionData={transitionData}
           currentSlideData={currentSlideData}
         />
-        <div className="  absolute z-20  h-full w-full">
+        <div className="absolute z-20 h-full w-full">
           <Header />
-          <div className=" flex h-full w-full grid-cols-10 flex-col md:grid">
-            <div className=" col-span-4 mb-3 flex h-full flex-1 flex-col justify-end px-5 md:mb-0 md:justify-center md:px-10">
-              <SlideInfo
-                transitionData={transitionData}
-                currentSlideData={currentSlideData}
-              />
+          <div className="flex h-full w-full grid-cols-10 flex-col md:grid">
+            <div className="col-span-4 mb-3 flex h-full flex-1 flex-col justify-end px-5 md:mb-0 md:justify-center md:px-10">
+              {/* Removed SlideInfo component as it is using removed data */}
             </div>
-            <div className=" col-span-6 flex h-full flex-1 flex-col justify-start p-4 md:justify-center md:p-10">
+            <div className="col-span-6 flex h-full flex-1 flex-col justify-start p-4 md:justify-center md:p-10">
               <Slides data={data} />
               <Controls
                 currentSlideData={currentSlideData}
@@ -77,87 +68,45 @@ export default function Home() {
 const sliderData = [
   {
     img: "/1.jpg",
-    location: "Switzrerland Apls",
-    description:
-      "The journey to Machu Picchu typically starts in the mountain city of Cusco, which was the capital city of the Inca Empire",
-    title: "SAINT ANTÖNEN",
   },
   {
     img: "/2.jpg",
-    title: "The Grand Canyon",
-    description:
-      "The earth's geological history opens before your eyes in a mile-deep chasm",
-    location: "Arizona",
   },
   {
     img: "/3.jpg",
-    title: "Masai Mara",
-    description:
-      "Wild animals in their natural environment, luxury safari lodges",
-    location: "Kenya",
   },
   {
     img: "/4.jpg",
-    title: "Angkor Wat",
-    description:
-      "A stunning ancient jungle city with hundreds of intricately constructed temples",
-    location: "Cambodia",
   },
   {
     img: "/5.jpg",
-    title: "Bali",
-    description:
-      "Tropical beaches, volcano hikes, ancient temples, and friendly people",
-    location: "Indonesia",
   },
   {
     img: "/6.jpg",
-    title: "Bali",
-    description:
-      "Tropical beaches, volcano hikes, ancient temples, and friendly people",
-    location: "Indonesia",
   },
   {
     img: "/7.jpg",
-    title: "Bali",
-    description:
-      "Tropical beaches, volcano hikes, ancient temples, and friendly people",
-    location: "Indonesia",
   },
   {
-    img: "/8.jpg",
-    title: "Bali",
-    description:
-      "Tropical beaches, volcano hikes, ancient temples, and friendly people",
-    location: "Indonesia",
+    img: "/8.webp",
   },
   {
     img: "/9.webp",
-    title: "Bali",
-    description:
-      "Tropical beaches, volcano hikes, ancient temples, and friendly people",
-    location: "Indonesia",
   },
   {
     img: "/10.jpg",
-    title: "Bali",
-    description:
-      "Tropical beaches, volcano hikes, ancient temples, and friendly people",
-    location: "Indonesia",
   },
   {
     img: "/11.webp",
-    title: "Bali",
-    description:
-      "Tropical beaches, volcano hikes, ancient temples, and friendly people",
-    location: "Indonesia",
   },
   {
     img: "/12.webp",
-    title: "Bali",
-    description:
-      "Tropical beaches, volcano hikes, ancient temples, and friendly people",
-    location: "Indonesia",
+  },
+  {
+    img: "/13.jpg",
+  },
+  {
+    img: "/14.webp",
   },
 ];
 
